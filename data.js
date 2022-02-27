adc84874ecf863a60c051d8e07fe6c530da80f75
Shubham Thakur <sdthakur@iu.edu>
2022-02-26 21:47:56 -0500

    Updated Report using report-curator

--- data.js
+++ data.js
@@ -1,1 +1,1 @@
-let data = {"views":{"dates":["02/15","02/17"],"count":[12,44],"uniques":[1,1]},"clones":{"dates":["02/17"],"count":[1],"uniques":[1]}};
+let data = {"views":{"dates":["02/15","02/17","02/18","02/21","02/22"],"count":[12,108,18,1,20],"uniques":[1,1,1,1,2]},"clones":{"dates":["02/17","02/18"],"count":[6,1],"uniques":[3,1]}};
